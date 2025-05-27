a74cc15512482e9d774d666a0dba81fae2270d10
Doug Hirlinger <doughirlinger@gmail.com>
2025-05-27 12:36:26 -0400

    added min max and step vars for base range

--- src/components/BaseMultiplier.jsx
+++ src/components/BaseMultiplier.jsx
@@ -10,6 +10,10 @@ export default function BaseMultiplier({
   const [multiplierMax, setMultiplierMax] = useState("10");
   const [multiplierStep, setMultiplierStep] = useState("0.1");
 
+  const [baseMin, setBaseMin] = useState("40");
+  const [baseMax, setBaseMax] = useState("10000");
+  const [baseStep, setBaseStep] = useState("10");
+
   const handleBaseChange = (e) => {
     let inputValue = e.target.value;
 
@@ -34,13 +38,42 @@ export default function BaseMultiplier({
       ></input>
       <input
         type="range"
-        max="10000"
-        min="40"
-        step="10"
+        max={baseMax}
+        min={baseMin}
+        step={baseStep}
         value={base}
         onChange={(e) => setBase(e.target.value)}
       />
       <div>
+        <div>
+          <label htmlFor="base-min">base min: </label>
+          <input
+            id="base-min"
+            type="text" // use text to fully control input
+            value={baseMin}
+            // maxLength={10}
+            style={{ width: "50px", fontSize: "14px" }}
+            onChange={(e) => setBaseMin(e.target.value)}
+          ></input>
+          <label htmlFor="base-max">base max: </label>
+          <input
+            id="base-max"
+            type="text" // use text to fully control input
+            value={baseMax}
+            // maxLength={10}
+            style={{ width: "50px", fontSize: "14px" }}
+            onChange={(e) => setBaseMax(e.target.value)}
+          ></input>
+          <label htmlFor="base-step">base step: </label>
+          <input
+            id="base-step"
+            type="text" // use text to fully control input
+            value={baseStep}
+            // maxLength={10}
+            style={{ width: "50px", fontSize: "14px" }}
+            onChange={(e) => setBaseStep(e.target.value)}
+          ></input>
+        </div>
         <span style={{ width: "100px" }}>Multiplier: </span>
         <input
           type="range"
@@ -52,7 +85,7 @@ export default function BaseMultiplier({
         />
         <span style={{ width: "50px" }}>{Number(multiplier).toFixed(3)}</span>
         <div>
-          <label htmlFor="base">multiplier min: </label>
+          <label htmlFor="multi-min">multiplier min: </label>
           <input
             id="multi-min"
             type="text" // use text to fully control input
@@ -61,7 +94,7 @@ export default function BaseMultiplier({
             style={{ width: "50px", fontSize: "14px" }}
             onChange={(e) => setMultiplierMin(e.target.value)}
           ></input>
-          <label htmlFor="base">multiplier max: </label>
+          <label htmlFor="multi-max">multiplier max: </label>
           <input
             id="multi-max"
             type="text" // use text to fully control input
@@ -70,7 +103,7 @@ export default function BaseMultiplier({
             style={{ width: "50px", fontSize: "14px" }}
             onChange={(e) => setMultiplierMax(e.target.value)}
           ></input>
-          <label htmlFor="base">multiplier step: </label>
+          <label htmlFor="multi-step">multiplier step: </label>
           <input
             id="multi-step"
             type="text" // use text to fully control input
