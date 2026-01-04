86c872fbd7ab6aa368d060b1dbac9728714fee4d
Doug Hirlinger <doughirlinger@gmail.com>
2026-01-04 10:33:46 -0500

    git biz

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { MidiProvider } from "./context/MidiContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MidiProvider>
+      <App />
+    </MidiProvider>
+  </StrictMode>
+);
