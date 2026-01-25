3663bebb5809f731b5a6b2f456b255126a14310f
Doug Hirlinger <doughirlinger@gmail.com>
2026-01-25 18:27:35 -0500

    added sticky bottom controls component and other small refactors

---
+++ src/components/StickyBottomControls.jsx
@@ -0,0 +1,32 @@
+export default function StickyBottomControls({
+  toggleSequencer,
+  seqIsPlaying,
+}) {
+  return (
+    <div className="sticky bottom-0 w-full flex p-2 border-t-8 border-x-0 border-b-0 border-t-[#242424] z-20 bg-[#E6A60D] m-0">
+      <button
+        className="w-[118px] min-w-[118px] border-stone-400 border"
+        onClick={toggleSequencer}
+      >
+        {seqIsPlaying ? "Stop" : "Play"}
+      </button>
+      <div className="flex items-center gap-1 ml-1.5 w-full justify-center">
+        <a href="#global-container">
+          <button className="text-sm text-[#E6A60D] w-[70px] min-w-[70px] flex justify-center">
+            Global
+          </button>
+        </a>
+        <a href="#frequency-grid">
+          <button className="text-sm text-cyan-500 w-[70px] min-w-[70px] flex justify-center-center">
+            Freq
+          </button>
+        </a>
+        <a href="#index-grid">
+          <button className="text-sm text-pink-500/90 w-[70px] min-w-[70px] flex justify-center">
+            Index
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
