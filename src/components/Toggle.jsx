e8ab8d4a15a1c1dec785313c4e315e9c2a22b345
Doug Hirlinger <doughirlinger@gmail.com>
2026-03-07 12:11:29 -0500

    fix tw prefix for span in toggle component

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -28,7 +28,7 @@ export default function Toggle({
           }}
           className="tw:opacity-0 tw:w-0 tw:h-0 tw:peer"
         />
-        <span className="tw:absolute tw:cursor-pointer tw:top-0 tw:left-0 tw:right-0 tw:bottom-0 tw:bg-black tw:rounded-[34px] tw:transition-all tw:duration-100 peer-checked:tw:bg-pink-500/90 peer-focus:tw:shadow-[0_0_1px_rgb(236_72_153/0.9)] before:tw:content-[''] before:tw:absolute before:tw:h-6.5 before:tw:w-6.5 before:tw:left-1 before:tw:bottom-1 before:tw:bg-white before:tw:rounded-full before:tw:transition-all before:tw:duration-100 peer-checked:before:tw:translate-x-6.5"></span>
+        <span className="tw:absolute tw:cursor-pointer tw:top-0 tw:left-0 tw:right-0 tw:bottom-0 tw:bg-black tw:rounded-[34px] tw:transition-all tw:duration-100 tw:peer-checked:bg-pink-500/90 tw:peer-focus:shadow-[0_0_1px_rgb(236_72_153/0.9)] tw:before:content-[''] tw:before:absolute tw:before:h-6.5 tw:before:w-6.5 tw:before:left-1 tw:before:bottom-1 tw:before:bg-white tw:before:rounded-full tw:before:transition-all tw:before:duration-100 tw:peer-checked:before:translate-x-6.5"></span>
       </label>
       <p className="tw:leading-[2.4] tw:font-bold">
         {camelCaseToNormal(capitalizeAllWords(id))}:
